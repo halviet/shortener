ccd2247299cd99edd693850bb304f539d91735ba
Egor Kramer <invscrl@gmail.com>
2024-07-21 19:56:26 +0700

    add: url handlers (hardcoded values for now)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,50 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /", ShortenURLHandle)
+	mux.HandleFunc("GET /{id}", GetURLHandle)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ShortenURLHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	defer r.Body.Close()
+	_, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("https://localhost:8080/EwHXdJfB"))
+}
+
+func GetURLHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if r.PathValue("id") == "EwHXdJfB" {
+		w.Header().Set("Location", "https://practicum.yandex.ru")
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+
+	http.Error(w, "Incorrect request", http.StatusBadRequest)
+}
